8371306c4f08fe742898a6d954c1306c6b9b7ebe
Ian Roberts <ian_p_roberts@btinternet.com>
2021-06-08 11:33:34 +0100

    Structured likelihood calculation

--- R/PhyloLikelihood.R
+++ R/PhyloLikelihood.R
@@ -5,7 +5,7 @@
 #'
 #' @param phylo object of class \code{phylo}
 #' @param effective.pop effective population size from which the sample is taken
-#' @param gen.length generation length of the sampled individuals
+#' @param gen.length mean generation length of the sampled individuals
 #'
 #' @return A vector (\code{log-likelihood}, \code{likelihood}) giving the log-likelihood and likelihood of \code{phylo}
 phylo.likelihood <- function(phylo, effective.pop, gen.length){
@@ -14,19 +14,18 @@ phylo.likelihood <- function(phylo, effective.pop, gen.length){
 
   node.heights <- node.depth.edgelength(phylo)  #Horizontal distance from root
   event.times <- sort(unique(node.heights))  #Times of events, root at time=0
-  time.increments <- event.times[2 : length(event.times)] - event.times[1 : (length(event.times) - 1)]
+  time.increments <- diff(event.times)
 
   #Augments phylo$edge to add edge "start" and "end" times
   aug.edge <- matrix(0,nrow = (2 * (n-1)), ncol = 4)
   aug.edge[,c(1,2)] <- phylo$edge
 
-  for (i in 1 : (2 * (n-1))){  ##### REPLACE LOOP WITH APPLY? #####
+  for (i in 1 : (2 * (n-1))){
     aug.edge[i,c(3,4)] <- c(node.heights[aug.edge[i,c(1,2)]])
   }
 
-  #Check how many edges in tree midway between event times
+  #Calculate number of edges in tree midway between event times
   check.time <- event.times[-length(event.times)] + 0.5 * time.increments
-
   k <- rep(0,length(time.increments))  #Number of edges in tree between events
   for (i in 1 : length(time.increments)){
     k[i] <- sum( (aug.edge[,3] < check.time[i]) & (aug.edge[,4] > check.time[i]) )
@@ -35,4 +34,97 @@ phylo.likelihood <- function(phylo, effective.pop, gen.length){
   likelihood <- 0  #Initialise log likelihood at 0
   likelihood <- sum(- (k * ( k-1) / (2 * lambda)) * time.increments) - (n-1) * log(lambda)
   c(likelihood, exp(likelihood))  #Output (log-likelihood, likelihood)
+}
+
+
+
+#' Calculates the likelihood of a structured coalescent tree
+#'
+#' Returns the likelihood and log-likelihood of a tree generated under the
+#' structured coalescent process
+#'
+#' @param phylo object of class \code{phylo} augmented with deme labels for each node
+#' @param effective.pop effective population sizes from which the sample is taken
+#' @param gen.length mean generation length of the sampled individuals
+#' @param migration.matrix matrix of migration rates between demes
+#'
+#' @return A vector (\code{log-likelihood}, \code{likelihood}) giving the log-likelihood and likelihood of \code{phylo}
+
+
+structured.likelihood <- function(phylo, effective.pop, gen.length, migration.matrix){
+  lambda <- effective.pop * gen.length
+  n <- length(phylo$tip.label) #Number of tips
+  n.demes <- dim(migration.matrix)[1] #Number of demes
+  n.migrations <- phylo$Nnode - n + 1 #Number of migration events
+
+
+  if (length(lambda) == 1){
+    lambda <- rep(lambda,n.demes)
+  }
+
+  #Identify node type (migration/coalescence/leaf)
+  leaf.nodes <- 1:n
+  migration.nodes <- phylo$edge[!phylo$edge[,1] %in% phylo$edge[duplicated(phylo$edge[,1]),1],1] #Unique elements in col 1 of edge matrix
+  coalescence.nodes <- ((n+1):(n.migrations+2*n-1))[!((n+1):(n.migrations+2*n-1) %in% migration.nodes)] #All remaining nodes
+
+  node.heights <- node.depth.edgelength(phylo)  #Horizontal distance from root
+  event.times <- sort(unique(node.heights))  #Times of events, root at time=0
+  time.increments <- diff(event.times)
+
+  #Augments phylo$edge to add edge "start" and "end" times
+  n.edges <- dim(phylo$edge)[1]
+  aug.edge <- matrix(0,nrow = n.edges, ncol = 4)
+  aug.edge[,c(1,2)] <- phylo$edge
+  aug.edge[,c(3,4)] <- node.heights[aug.edge[,c(1,2)]]
+
+  #Number of lineages in each deme between each event time
+  check.time <- event.times[-length(event.times)] + 0.5 * time.increments
+  k <- matrix(0, nrow = length(check.time), ncol = n.demes)
+  for (i in 1 : length(time.increments)){
+    current.edges <- which((aug.edge[,3] < check.time[i]) & (aug.edge[,4] > check.time[i]) )
+    for (j in 1 : n.demes){
+      k[i,j] <- sum(phylo$node.deme[aug.edge[current.edges,2]] == j)
+    }
+  }
+
+  #Number of coalescence events in each deme
+  c <- rep(0,n.demes)
+  for (j in 1 : n.demes){
+    c[j] <- sum(phylo$node.deme[coalescence.nodes] == j)
+  }
+
+  #Number of migration events between each pair of demes
+  m <- matrix(0,nrow = n.demes, ncol = n.demes)
+  for (nodes in migration.nodes){
+    i <- phylo$node.deme[aug.edge[which(aug.edge[,1] == nodes),2]]
+    j <- phylo$node.deme[nodes]
+    m[i,j] <- m[i,j] + 1
+  }
+
+  #Likelihood computation
+  likelihood <- 0
+  likelihood <- - sum(rowSums(t(t(k * (k-1)) / (2 * lambda)) + t(t(k) * rowSums(migration.matrix))) * time.increments) -
+    sum(c * log(lambda)) + sum(log(migration.matrix ^ m))
+  likelihood
+
+  #not.root <- (1:(n.migrations+2*n-1))[-(n+1)]
+  #likelihood <- 0
+  #for (r in 1:length(time.increments)){
+  #  for (i in 1 : n.demes){
+  #    likelihood <- likelihood - k[r,i] * (k[r,i] - 1) / (2 * lambda[i]) * time.increments[r]
+  #
+  #    for (j in (1 : n.demes)[-i]){
+  #      likelihood <- likelihood - k[r,i] * migration.matrix[i,j] * time.increments[r]
+  #    }
+  #  }
+  #  }
+  #
+  #for (i in 1 : n.demes){
+  #  likelihood <- likelihood - c[i] * log(lambda[i])
+  #
+  #  for (j in (1:n.demes)[-i]){
+  #    likelihood <- likelihood + m[i,j] * log(migration.matrix[i,j])
+  #  }
+  #}
+
 }
