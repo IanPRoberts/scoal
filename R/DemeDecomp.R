4d9a1810c3b760155b30e62d45e329755bebbb68
Ian Roberts <ian_p_roberts@btinternet.com>
2022-02-28 09:20:18 +0000

    Improved deme decomp - which statement on vector of active nodes instead of ED[,1]

--- R/DemeDecomp.R
+++ R/DemeDecomp.R
@@ -4,32 +4,38 @@ deme.decomp <- function(ED, n.deme, node.indices){
 
   k <- matrix(0, nrow = length(event.times) - 1, ncol = n.deme)
   root.row <- node.indices[ED[is.na(ED[,2]),1]]
+  active.nodes <- ED[root.row, 3:4]
   k[1,ED[root.row, 5]] <- 2
 
   for (i in 2 : (length(event.times) - 1)){
-    current.rows <- which(ED[,6] == event.times[i])
-
     k[i,] <- k[i-1,]
+    active.rows <- node.indices[active.nodes]
+    current.indices <- which(ED[active.rows, 6] == event.times[i])
+    current.rows <- active.rows[current.indices]
+
     if (length(current.rows) > 1){ #Multiple leaves
       for (j in current.rows){
         current.deme <- ED[j, 5]
         k[i, current.deme] <- k[i, current.deme] - 1
       }
     } else{
-      if (is.na(ED[current.rows, 3])){ #Leaf
+      if (anyNA(ED[current.rows, 3])){ #Leaf
         current.deme <- ED[current.rows, 5]
         k[i, current.deme] <- k[i, current.deme] - 1
-      } else if (!is.na(ED[current.rows, 4])){ #Coalescence
+      } else if (!anyNA(ED[current.rows, 4])){ #Coalescence
         current.deme <- ED[current.rows, 5]
         k[i, current.deme] <- k[i, current.deme] + 1
+        active.nodes <- c(active.nodes, ED[current.rows, 3:4])
       } else{ #Migration
         current.deme <- ED[current.rows, 5]
         k[i, current.deme] <- k[i, current.deme] - 1
         current.child <- node.indices[ED[current.rows, 3]]
         child.deme <- ED[current.child, 5]
         k[i, child.deme] <- k[i, child.deme] + 1
+        active.nodes <- c(active.nodes, ED[current.rows, 3])
       }
     }
+    active.nodes <- active.nodes[-current.indices]
   }
 
   return(k)
