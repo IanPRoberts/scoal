6e5b3a3e4744bda86bea48b6da5fbb6a10aa5bbc
Ian Roberts <ian_p_roberts@btinternet.com>
2022-04-12 17:30:19 +0100

    Fixed tests for new function inputs (adding node.indices)

--- tests/testthat/test.MCMC.mig.number.R
+++ tests/testthat/test.MCMC.mig.number.R
@@ -4,9 +4,12 @@ test_that("Migration pair birth adds two migrations",{
   data <- matrix(c(1:10, rep(2022, 10), rep(1, 4), rep(2,4), rep(3, 2)),nrow = 10, ncol = 3)
   homo.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(homo.phylo)
+  node.indices <- 1 : dim(ED)[1]
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for(i in 1 : 5){
-    ED <- ed.mig.pair.birth(ED, 3)$ED
+    proposal <- ed.mig.pair.birth(ED, 3, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_equal(M+2, M.2)
     M <- M.2
@@ -15,9 +18,12 @@ test_that("Migration pair birth adds two migrations",{
   data <- matrix(c(1:10, rep(2022, 4), rep(2020, 5), 2019, rep(1, 4), rep(2,4), rep(3, 2)),nrow = 10, ncol = 3)
   hetero.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(hetero.phylo)
+  node.indices <- 1 : dim(ED)[1]
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for(i in 1 : 5){
-    ED <- ed.mig.pair.birth(ED, 3)$ED
+    proposal <- ed.mig.pair.birth(ED, 3, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_equal(M+2, M.2)
     M <- M.2
@@ -28,12 +34,17 @@ test_that("Migration pair death remove zero or two migrations",{
   data <- matrix(c(1:10, rep(2022, 10), rep(1, 4), rep(2,4), rep(3, 2)),nrow = 10, ncol = 3)
   homo.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(homo.phylo)
+  node.indices <- 1 : dim(ED)[1]
   for(i in 1 : 20){
-    ED <- ed.mig.pair.birth(ED, 3)$ED
+    proposal <- ed.mig.pair.birth(ED, 3, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
   }
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for (j in 1 : 5){
-    ED <- ed.mig.pair.death(ED, 3)$ED
+    proposal <- ed.mig.pair.death(ED, 3, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_true(M.2 %in% c(M, M-2))
     M <- M.2
@@ -42,12 +53,17 @@ test_that("Migration pair death remove zero or two migrations",{
   data <- matrix(c(1:10, rep(2022, 4), rep(2020, 5), 2019, rep(1, 4), rep(2,4), rep(3, 2)),nrow = 10, ncol = 3)
   hetero.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(hetero.phylo)
+  node.indices <- 1 : dim(ED)[1]
   for(i in 1 : 20){
-    ED <- ed.mig.pair.birth(ED, 3)$ED
+    proposal <- ed.mig.pair.birth(ED, 3, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
   }
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for(i in 1 : 5){
-    ED <- ed.mig.pair.death(ED, 3)$ED
+    proposal <- ed.mig.pair.death(ED, 3, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_true(M.2 %in% c(M, M-2))
     M <- M.2
@@ -58,9 +74,12 @@ test_that("Migration birth adds zero or one migration",{
   data <- matrix(c(1:10, rep(2022, 10), rep(1, 4), rep(2,4), rep(3, 2)),nrow = 10, ncol = 3)
   homo.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(homo.phylo)
+  node.indices <- 1 : dim(ED)[1]
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for(i in 1 : 5){
-    ED <- ed.mig.birth.4(ED, 3, TRUE)$ED
+    proposal <- ed.mig.birth(ED, 3, TRUE, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_true(M.2 %in% c(M, M+1))
     M <- M.2
@@ -69,9 +88,12 @@ test_that("Migration birth adds zero or one migration",{
   data <- matrix(c(1:10, rep(2022, 4), rep(2020, 5), 2019, rep(1, 4), rep(2,4), rep(3, 2)),nrow = 10, ncol = 3)
   hetero.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(hetero.phylo)
+  node.indices <- 1 : dim(ED)[1]
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for(i in 1 : 5){
-    ED <- ed.mig.birth.4(ED, 3, TRUE)$ED
+    proposal <- ed.mig.birth(ED, 3, TRUE, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_true(M.2 %in% c(M, M+1))
     M <- M.2
@@ -82,12 +104,17 @@ test_that("Migration death removes zero or one migration",{
   data <- matrix(c(1:10, rep(2022, 10), rep(1, 4), rep(2,4), rep(3, 2)),nrow = 10, ncol = 3)
   homo.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(homo.phylo)
+  node.indices <- 1 : dim(ED)[1]
   for (i in 1 : 20){
-    ED <- ED <- ed.mig.birth.4(ED, 3, TRUE)$ED
+    proposal <- ed.mig.birth(ED, 3, TRUE, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
   }
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for(i in 1 : 5){
-    ED <- ed.mig.death.4(ED, 3, TRUE)$ED
+    proposal <- ed.mig.death(ED, 3, TRUE, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_true(M.2 %in% c(M, M-1))
     M <- M.2
@@ -97,11 +124,15 @@ test_that("Migration death removes zero or one migration",{
   hetero.phylo <- Structured.sim(data, 1, 1, 3, matrix(c(0, rep(1/10, 3), 0, rep(1/10, 3), 0),3,3), FALSE)
   ED <- phylo.to.ed(hetero.phylo)
   for (i in 1 : 20){
-    ED <- ED <- ed.mig.birth.4(ED, 3, TRUE)$ED
+    proposal <- ed.mig.birth(ED, 3, TRUE, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
   }
   M <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
   for(i in 1 : 5){
-    ED <- ed.mig.death.4(ED, 3, TRUE)$ED
+    proposal <- ed.mig.death(ED, 3, TRUE, node.indices)
+    ED <- proposal$ED
+    node.indices <- proposal$node.indices
     M.2 <- sum((is.na(ED[,4])) & (!is.na(ED[,3])))
     expect_true(M.2 %in% c(M, M-1))
     M <- M.2
