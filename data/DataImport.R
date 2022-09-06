66dbac105d0856d7e6623ec66bbe48b0de9325ae
IanPRoberts <ian_p_roberts@btinternet.com>
2022-09-06 14:12:25 +0100

    Revised data import for Harris (2010) and Didelot (2015) phylogenies

--- data/DataImport.R
+++ data/DataImport.R
@@ -1,49 +1,41 @@
 f <- function(){
 
-didelot2015phylo <- ape::read.nexus("data/didelot2015.nex")
-node.color <- read.table("Data/didelot2015.nex", skip = 4, nrows = 260, comment.char = "")
-node.color <- sub('...', '', gsub('[^0-9.]','',node.color[,]))
-n.leaf.deme <- length(unique(node.color))
-leaf.deme <- rep(0, 260)
+  phylo <- ape::read.nexus("data/didelot2015.nex")
+  node.color <- read.table("data/didelot2015.nex", skip = 4, nrows = 260, comment.char = "")
+  tip.label <- gsub('.{17}$', '', node.color[,1])
+  tip.order <- sapply(1:260, function(x) which(tip.label == phylo$tip.label[x]))
 
-for (i in 1:260){
-  leaf.deme[i] <- (1:n.leaf.deme)[which(unique(node.color) == node.color[i])]
-}
-leaf.deme <- as.numeric(leaf.deme)
+  node.color <- as.factor(gsub('^.{15}|.$', '', node.color[,1]))
+  leaf.deme <- sapply(1:260, function(x) which(unique(node.color) == node.color[x]))
 
-didelot2015phylo$node.deme <- c(node.color, rep(0, 259))
-didelotED <- phylo.to.ed(didelot2015phylo)
-View(didelotED)
-
-
-save(didelot2015phylo, file = "data/didelot2015phylo.rda")
+  phylo$node.deme <- c(leaf.deme[tip.order], rep(0, 259))
+  ED <- phylo.to.ed(didelot2015phylo)
+  saveRDS(phylo, file = "data/didelot2015phylo.RDS")
+  saveRDS(ED, "data/didelot2015ED.RDS")
 }
 
 g <- function(){
-  harris2010phylo <- ape::read.tree("data/harris2010.nwk")
-  harris2010NodeData <- read.csv("data/harris2010.csv")
-  demes <- levels(as.factor(harris2010NodeData$Location)) #levels(harris2010NodeData$Location)
-  n.deme <- length(demes)
+  phylo <- ape::read.tree("data/harris2010.nwk")
+  leaf.data <- read.csv("data/harris2010.csv")
 
-  n.leaf <- dim(harris2010NodeData)[1]
-  node.deme <- rep(n.deme+1, 2 * n.leaf - 1)
-  for (i in 1:n.leaf){
-    node.deme[i] <- (1:n.deme)[which(demes == harris2010NodeData$Location[i])]
-  }
+  demes <- unique(leaf.data$Location)
+  n.deme <- 5
+  leaf.deme <- sapply(1:58, function(x) which(demes == leaf.data))
 
-  node.deme[1:n.leaf] <- node.deme[as.numeric(harris2010phylo$tip.label)]  #Reorder to satisfy weird ordering of tip.labels
+  tip.order <- as.numeric(phylo$tip.label)
 
-  harris2010phylo$node.deme <- node.deme
-  harrisED <- phylo.to.ed(harris2010phylo)
-
+  phylo$node.deme <- c(leaf.deme[tip.order], rep(0, 57))
+  ED <- phylo.to.ed(phylo)
+  saveRDS(phylo, file = "data/harris2010phylo.RDS")
+  saveRDS(ED, "data/harris2010ED.RDS")
 
 
   ### Convert to .nex file
-  library(tibble)
-  library(treeio)
-  tree=read.tree('data/harris2010.nwk')
-  table=read.table('data/harris2010.csv',sep=',',header = T)
-  ti=tibble(label=as.character(table[,1]),location=table[,3])
-  td=full_join(tree,ti,by='label')
-  write.beast(td,'data/harris2010.nex')
+  # library(tibble)
+  # library(treeio)
+  # tree=read.tree('data/harris2010.nwk')
+  # table=read.table('data/harris2010.csv',sep=',',header = T)
+  # ti=tibble(label=as.character(table[,1]),location=table[,3])
+  # td=full_join(tree,ti,by='label')
+  # write.beast(td,'data/harris2010.nex')
 }
