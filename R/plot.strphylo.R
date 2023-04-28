4e962a75966d263964b735a3367ff6039920386e
IanPRoberts <ian_p_roberts@btinternet.com>
2023-04-28 15:56:38 +0100

    structured_plot.R renamed and structured.plot() no longer exported

--- R/plot.strphylo.R
+++ R/plot.strphylo.R
@@ -1,10 +1,29 @@
+#' @export
+plot.strphylo <- function(x, time_axis = TRUE, n_deme = NULL, root_time = NULL, ...){
+  if (is.null(n_deme)){
+    n_deme <- max(x$node.deme)
+  }
+
+  color.palette <- c(rainbow(n_deme), "black")
+  x$node.deme[x$node.deme == 0] <- n_deme + 1
+
+  edge_color <- color.palette[x$node.deme[x$edge[,2]]]
+
+  plot.phylo(x, edge.color = edge_color, edge.width = 2, ...)
+
+  if (time_axis){
+    if (is.null(root_time)){
+      root_time <- 0
+    }
+    axisPhylo(root.time = root_time, backward = FALSE)
+  }
+}
+
 #' Plot Structured Coalescent Tree
 #'
 #' Plots a structured coalescent tree input as a phylo object augmented with node demes
 #'
 #' @param phylo phylo object augmented with node demes
-#'
-#' @export
 
 structured.plot <- function(x, n_deme = NA, time_axis = FALSE, root_time = NA, ...){
   if (! ('phylo' %in% class(x))){
@@ -32,28 +51,6 @@ structured.plot <- function(x, n_deme = NA, time_axis = FALSE, root_time = NA, .
 
   if (time_axis){
     if (is.na(root_time)){
-      root_time <- 0
-    }
-    axisPhylo(root.time = root_time, backward = FALSE)
-  }
-}
-
-
-#' @export
-plot.strphylo <- function(x, n_deme = NULL, time_axis = FALSE, root_time = NULL, ...){
-  if (is.null(n_deme)){
-    n_deme <- max(x$node.deme)
-  }
-
-  color.palette <- c(rainbow(n_deme), "black")
-  x$node.deme[x$node.deme == 0] <- n_deme + 1
-
-  edge_color <- color.palette[x$node.deme[x$edge[,2]]]
-
-  plot.phylo(x, edge.color = edge_color, edge.width = 2, ...)
-
-  if (time_axis){
-    if (is.null(root_time)){
       root_time <- 0
     }
     axisPhylo(root.time = root_time, backward = FALSE)
